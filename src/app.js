0facb4cba22f7eb154854b01a04ef328b845a719
Omar Hidalgo <omar_ehr@outlook.com>
2024-04-30 03:31:53 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let domainName = () => {
+  const domains = document.getElementById("domains");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".io", ".es"];
 
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          domains.innerHTML += `<p> ${pronoun}${adj}${noun}${extension}<p>`;
+        }
+      }
+    }
+  }
+};
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello!");
+  domainName();
 };
